a0aa314393b5743f5b2b013a0f44e9a9df19ac1c
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-03-29 13:03:54 +0200

    Add Chapter 01, exercise 10

---
+++ src/chapter01/ch01ex10/main.go
@@ -0,0 +1,47 @@
+// Modify fetchall to print output to file
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	f, err := os.Create("./fetchall.dat")
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't create file: %v\n", err)
+	}
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Fprintln(f, <-ch) // receive from channel ch
+	}
+
+	fmt.Fprintf(f, "%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
