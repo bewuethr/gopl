0678f9dc5300fedf7424bac163ed815caedb8f02
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-06-13 01:24:47 -0400

    Add Chapter 04, exercise 4

---
+++ src/chapter04/ch04ex04/main.go
@@ -0,0 +1,32 @@
+// ch04ex04 implements a single pass rotate function.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions.
+	rotate(s, 2)
+	fmt.Println(s) // "[2 3 4 5 0 1]"
+
+	// Rotate s right by three positions.
+	rotate(s, -3)
+	fmt.Println(s) // "[4 5 0 1 2 3]"
+}
+
+// rotate rotates a slice of ints, where the rotation is left for positive
+// amounts and right for negative amounts.
+func rotate(s []int, dist int) {
+	// Get index offset (the element at which index goes into index 0?)
+	offset := (dist + len(s)) % len(s)
+
+	res := make([]int, len(s))
+
+	for i := range s {
+		res[i] = s[(i+offset)%len(s)]
+	}
+
+	copy(s, res)
+}
