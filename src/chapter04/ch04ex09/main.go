1267122354cba29d88dec1db1b9e387f79344e2c
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-07-19 00:51:04 -0400

    Add Chapter 04, exercise 9

---
+++ src/chapter04/ch04ex09/main.go
@@ -0,0 +1,32 @@
+// ch04ex09 counts the frequency of each word in an input text file.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "wordfreq: no file argument provided")
+		os.Exit(1)
+	}
+	f, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "wordfreq: %v\n", err)
+		os.Exit(1)
+	}
+	input := bufio.NewScanner(f)
+	input.Split(bufio.ScanWords)
+
+	freq := make(map[string]int)
+
+	for input.Scan() {
+		freq[input.Text()]++
+	}
+
+	for w, c := range freq {
+		fmt.Printf("%s\t%d\n", w, c)
+	}
+}
