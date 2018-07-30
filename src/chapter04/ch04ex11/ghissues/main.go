308ad943271092860f565a95b4ca8fdf893b8acc
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-07-29 23:28:23 -0400

    Add Chapter 04, exercise 11

--- src/chapter04/ch04ex11/ghissues/main.go
+++ src/chapter04/ch04ex11/ghissues/main.go
@@ -11,10 +11,15 @@ import (
 	"chapter04/ch04ex11/github"
 )
 
+// TODO DRY the create, update and close requests
+// TODO Read arguments with command line flags
+// TODO Open editor if title/body is missing in arguments
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "No arguments provided\n"+
-			"Usage: ghissues read OWNER REPO NUMBER\n")
+			"Usage: ghissues <command> [<args>]\n"+
+			"where <command> is \"read\", \"create\" or \"update\"\n")
 		os.Exit(1)
 	}
 
@@ -35,13 +40,64 @@ func main() {
 			log.Fatal(err)
 		}
 		printIssue(result)
+
+	case "create":
+		if len(os.Args) < 6 {
+			fmt.Fprint(os.Stderr, "Not enough arguments provided\n"+
+				"Usage: ghissues create OWNER REPO TITLE BODY\n")
+			os.Exit(1)
+		}
+		owner, repo, title, body := os.Args[2], os.Args[3], os.Args[4], os.Args[5]
+		result, err := github.CreateIssue(owner, repo, title, body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Issue successfully created at %s\n", result.URL)
+
+	case "update":
+		if len(os.Args) < 7 {
+			fmt.Fprint(os.Stderr, "Not enough arguments provided\n"+
+				"Usage: ghissues update OWNER REPO NUMBER TITLE BODY\n")
+			os.Exit(1)
+		}
+		owner, repo, title, body := os.Args[2], os.Args[3], os.Args[5], os.Args[6]
+		number, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+		result, err := github.UpdateIssue(owner, repo, number, title, body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Issue at %s successfully updated\n", result.URL)
+
+	case "close":
+		if len(os.Args) < 5 {
+			fmt.Fprint(os.Stderr, "Not enough arguments provided\n"+
+				"Usage: ghissues close OWNER REPO NUMBER\n")
+			os.Exit(1)
+		}
+		owner, repo := os.Args[2], os.Args[3]
+		number, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			log.Fatal(err)
+		}
+		result, err := github.CloseIssue(owner, repo, number)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Issue at %s successfully closed\n", result.URL)
+
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command '%s'\n"+
-			"Usage: ghissues read OWNER REPO NUMBER\n", os.Args[1])
+			"Usage: ghissues <command> [<args>]\n"+
+			"where <command> is \"read\", \"create\", \"update\" or \"close\"\n", os.Args[1])
 		os.Exit(1)
 	}
 }
 
 func printIssue(issue *github.Issue) {
-	fmt.Printf("%+v\n", *issue)
+	fmt.Printf("#%d - %s\n%s\n\n", issue.Number, issue.Title, issue.HTMLURL)
+	fmt.Printf("Created: %s\nUser: %s\n\n", issue.CreatedAt, issue.User.Login)
+	fmt.Printf("State: %s\n\n%s\n", issue.State, issue.Body)
 }
--- src/chapter04/ch04ex11/github/github.go
+++ src/chapter04/ch04ex11/github/github.go
@@ -2,13 +2,26 @@
 package github
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 const BaseAPIURL = "https://api.github.com"
+
+var authToken string
+
+func init() {
+	var ok bool
+	authToken, ok = os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		log.Fatal("Environment variable GITHUB_TOKEN is not set")
+	}
+}
 
 type Issue struct {
 	Number    int
@@ -25,6 +38,19 @@ type User struct {
 	HTMLURL string `json:"html_url"`
 }
 
+type CreateRequest struct {
+	Title string `json:"title"`
+	Body  string `json:"body"` // in Markdown format
+}
+
+type CreateResponse struct {
+	URL string
+}
+
+type CloseRequest struct {
+	State string `json:"state"`
+}
+
 // ReadIssue gets an issue from a repo.
 func ReadIssue(owner, repo string, number int) (*Issue, error) {
 	req, err := http.NewRequest(http.MethodGet,
@@ -37,13 +63,118 @@ func ReadIssue(owner, repo string, number int) (*Issue, error) {
 	resp, err := http.DefaultClient.Do(req)
 
 	// We must close resp.Body on all execution paths.
-	// (Chapter 5 presents 'defer', which makes this simpler.)
 	if resp.StatusCode != http.StatusOK {
 		resp.Body.Close()
 		return nil, fmt.Errorf("search query failed: %s", resp.Status)
 	}
 
 	var result Issue
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
+
+// CreateIssue creates a new issue in the specified repo and returns the
+// response struct with the URL if successful.
+func CreateIssue(owner, repo, title, body string) (*CreateResponse, error) {
+	reqBody := CreateRequest{
+		Title: title,
+		Body:  body,
+	}
+	jsonBody, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest(http.MethodPost,
+		fmt.Sprintf("%s/repos/%s/%s/issues", BaseAPIURL, owner, repo),
+		bytes.NewBuffer(jsonBody))
+	req.Header.Set("Accept", "application/vnd.github.v3.+json")
+	req.Header.Set("User-Agent", "gopl-ch04ex11-cli-tool")
+	req.Header.Set("Authorization", "Token "+authToken)
+	resp, err := http.DefaultClient.Do(req)
+
+	if resp.StatusCode != http.StatusCreated {
+		resp.Body.Close()
+		return nil, fmt.Errorf("create query failed: %s", resp.Status)
+	}
+
+	var createResponse CreateResponse
+	if err := json.NewDecoder(resp.Body).Decode(&createResponse); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &createResponse, nil
+}
+
+// UpdateIssue updates an issue and returns the response struct with the URL if
+// successful.
+func UpdateIssue(owner, repo string, number int, title, body string) (*CreateResponse, error) {
+	reqBody := CreateRequest{
+		Title: title,
+		Body:  body,
+	}
+	jsonBody, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest(http.MethodPatch,
+		fmt.Sprintf("%s/repos/%s/%s/issues/%d", BaseAPIURL, owner, repo, number),
+		bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github.v3.+json")
+	req.Header.Set("User-Agent", "gopl-ch04ex11-cli-tool")
+	req.Header.Set("Authorization", "Token "+authToken)
+	resp, err := http.DefaultClient.Do(req)
+
+	// We must close resp.Body on all execution paths.
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result CreateResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+	resp.Body.Close()
+	return &result, nil
+}
+
+// CloseIssue closes an issue and returns the response struct with the URL if
+// successful.
+func CloseIssue(owner, repo string, number int) (*CreateResponse, error) {
+	reqBody := CloseRequest{
+		State: "closed",
+	}
+	jsonBody, err := json.Marshal(reqBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest(http.MethodPatch,
+		fmt.Sprintf("%s/repos/%s/%s/issues/%d", BaseAPIURL, owner, repo, number),
+		bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github.v3.+json")
+	req.Header.Set("User-Agent", "gopl-ch04ex11-cli-tool")
+	req.Header.Set("Authorization", "Token "+authToken)
+	resp, err := http.DefaultClient.Do(req)
+
+	// We must close resp.Body on all execution paths.
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("search query failed: %s", resp.Status)
+	}
+
+	var result CreateResponse
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
 		resp.Body.Close()
 		return nil, err
