f59f0000c425def9e7ff33d2af107779dbe53920
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-08-04 15:18:32 -0400

    Add Chapter 04, exercise 13
    This uses the API at voku.xyz instead of OMDb, as the latter requires a
    subscription to be able to use the poster API.

---
+++ src/chapter04/ch04ex13/main.go
@@ -0,0 +1,94 @@
+// ch04ex13 queries the IMDb API at voku.xyz for movie titles and fetches the
+// poster for the movie supplied as a command line argument.
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+const apiBaseURL = "https://voku.xyz/imdb/api"
+
+type movie struct {
+	Name      string
+	PosterURL string `json:"poster"`
+	Error     string
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s MOVIE\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	movie, err := getMovie(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not get movie information: %s\n", err)
+		os.Exit(1)
+	}
+
+	err = getPoster(movie)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not get poster: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Poster stored in %s.jpg\n", movie.Name)
+}
+
+func getMovie(name string) (*movie, error) {
+	url := fmt.Sprintf("%s?q=%s", apiBaseURL, url.QueryEscape(name))
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	if string(bodyBytes) == `{"Error":"No results found"}` ||
+		strings.HasPrefix(string(bodyBytes), `"result not found.`) {
+		return nil, errors.New("no results found")
+	}
+
+	var m movie
+	if err := json.Unmarshal(bodyBytes, &m); err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func getPoster(m *movie) error {
+	resp, err := http.Get(m.PosterURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.ContentLength == 0 {
+		return errors.New("Could not fetch poster")
+	}
+
+	file, err := os.Create(m.Name + ".jpg")
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	file.Close()
+	return nil
+}
