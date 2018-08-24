7fa4087a9f18b05633fd72e954c75d9de7257f12
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-08-24 00:20:48 -0400

    Add Chapter 05, exercise 4

---
+++ src/chapter05/ch05ex04/main.go
@@ -0,0 +1,54 @@
+// Ch05ex04 extracts image, script and style sheet links.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ch05ex04: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+
+	if n.Type == html.ElementNode {
+		if n.Data == "img" || n.Data == "script" {
+			for _, a := range n.Attr {
+				if a.Key == "src" {
+					links = append(links, a.Val)
+				}
+			}
+		}
+		if n.Data == "link" {
+			var rel, target string
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					target = a.Val
+				} else if a.Key == "rel" {
+					rel = a.Val
+				}
+			}
+			if rel == "stylesheet" {
+				links = append(links, target)
+			}
+		}
+	}
+
+	links = visit(links, n.FirstChild)
+	links = visit(links, n.NextSibling)
+	return links
+}
