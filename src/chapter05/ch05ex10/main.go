b4ba926f9b3ba82fc6d727bf3782b8d3ba04fd7d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-09-11 01:00:42 -0400

    Add Chapter 05, exercise 10

---
+++ src/chapter05/ch05ex10/main.go
@@ -0,0 +1,55 @@
+// ch05ex10 rewrites topoSort to use maps instead of slices and eliminates the
+// initial sort.
+package main
+
+import "fmt"
+
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string]map[string]bool{
+	"algorithms": {"data structures": true},
+	"calculus":   {"linear algebra": true},
+
+	"compilers": {
+		"data structures":       true,
+		"formal languages":      true,
+		"computer organization": true,
+	},
+
+	"data structures":       {"discrete math": true},
+	"databases":             {"data structures": true},
+	"discrete math":         {"intro to programming": true},
+	"formal languages":      {"discrete math": true},
+	"networks":              {"operating systems": true},
+	"operating systems":     {"data structures": true, "computer organization": true},
+	"programming languages": {"data structures": true, "computer organization": true},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string]map[string]bool) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items map[string]bool)
+
+	visitAll = func(items map[string]bool) {
+		for key := range items {
+			if !seen[key] {
+				seen[key] = true
+				visitAll(m[key])
+				order = append(order, key)
+			}
+		}
+	}
+
+	keys := make(map[string]bool)
+	for key := range m {
+		keys[key] = true
+	}
+
+	visitAll(keys)
+	return order
+}
