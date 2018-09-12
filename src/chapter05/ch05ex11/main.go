e71492f1acddacae358ca5ad4696777c82b54ee9
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-09-12 00:55:29 -0400

    Add Chapter 05, exercise 11

---
+++ src/chapter05/ch05ex11/main.go
@@ -0,0 +1,70 @@
+// ch05ex11 extends topoSort to report cycles.
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+	"linear algebra":        {"calculus"}, // circular dependency
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	// if a key exists with value false, it counts as temporarily marked (used
+	// to detect loops); after a node has been appended to order, it is marked
+	// with true.
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			s, ok := seen[item]
+			if ok && !s {
+				// We've found a loop
+				fmt.Fprintf(os.Stderr, "Found a loop involving %v\n", item)
+				os.Exit(1)
+			}
+			if !s {
+				seen[item] = false // temporary mark
+				visitAll(m[item])
+				order = append(order, item)
+				seen[item] = true // definitive mark
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
