c7e75b4e2d67e8a63b5db251d49d5863f0839fe4
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-09-18 00:20:54 -0400

    Add Chapter 05, exercise 16

---
+++ src/chapter05/ch05ex16/main.go
@@ -0,0 +1,31 @@
+// ch05ex16 implements strings.Join as a variadic function.
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	fmt.Println(join("X", ""))
+	fmt.Println(join("X", "abc"))
+	fmt.Println(join("X", "abc", "def"))
+}
+
+// join concatenates the elements of a to create a single string. The separator
+// string sep is placed between elements in the resulting string.
+func join(sep string, a ...string) string {
+	switch len(a) {
+	case 0:
+		return ""
+	case 1:
+		return a[0]
+	}
+
+	res := bytes.NewBufferString(a[0])
+	for _, s := range a[1:] {
+		res.WriteString(sep + s)
+	}
+
+	return res.String()
+}
