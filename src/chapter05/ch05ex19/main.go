99139aaf06ebd20eef0ae9403c50eef5c77cdec4
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-09-25 00:48:36 -0400

    Add Chapter 05, exercise 19

---
+++ src/chapter05/ch05ex19/main.go
@@ -0,0 +1,17 @@
+// ch05ex19 implements a function without return statement that returns a
+// non-zero value.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(magicFunc())
+}
+
+func magicFunc() (val int) {
+	defer func() {
+		recover()
+		val = 1
+	}()
+	panic("panic!")
+}
