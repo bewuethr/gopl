2875da513429cc207a304c97a82d4b02e90af82a
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-10-03 01:38:02 -0400

    Add Chapter 07, exercise 4

---
+++ chapter07/ch07ex04/main.go
@@ -0,0 +1,72 @@
+// ch07ex04 implements NewReader and a simple string reader to use with the
+// HTML parser from exercise 5.2.
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+// MyReader implements an io.Reader.
+type MyReader struct {
+	p   []byte
+	idx int
+}
+
+func (r *MyReader) Read(p []byte) (int, error) {
+	read := 0
+	for i := range p {
+		if r.idx == len(r.p) {
+			return read, io.EOF
+		}
+		p[i] = r.p[r.idx]
+		read++
+		r.idx++
+	}
+	return read, nil
+}
+
+// NewReader reuturns a pointer to an instance of MyReader.
+func NewReader(s string) io.Reader {
+	r := MyReader{
+		p:   []byte(s),
+		idx: 0,
+	}
+	return &r
+}
+
+func main() {
+	inBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ch07ex04: %v\n", err)
+		os.Exit(1)
+	}
+	reader := NewReader(string(inBytes))
+	doc, err := html.Parse(reader)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ch07ex04: %v\n", err)
+		os.Exit(1)
+	}
+	for name, count := range visit(make(map[string]int), doc) {
+		fmt.Printf("%s:\t%d\n", name, count)
+	}
+}
+
+// visit updates the mapping for the element count and returns the result.
+func visit(elCount map[string]int, n *html.Node) map[string]int {
+	if n == nil {
+		return elCount
+	}
+
+	if n.Type == html.ElementNode {
+		elCount[n.Data]++
+	}
+
+	elCount = visit(elCount, n.FirstChild)
+	elCount = visit(elCount, n.NextSibling)
+	return elCount
+}
