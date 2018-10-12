7475c501f3d0d98a0ceef0714fc86e94dcb01831
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-10-12 01:20:21 -0400

    Add Chapter 07, exercise 7

--- chapter07/ch07ex06/main.go
+++ chapter07/ch07ex06/main.go
@@ -9,6 +9,8 @@ import (
 	"gopl/chapter07/ch07ex06/tempconv"
 )
 
+// Exercise 7.7: The help message will say "20°C" because it uses
+// tempconv.Celsius.String(). It is triggered by flag.PrintDefaults().
 var temp = tempconv.CelsiusFlag("temp", 20.0, "the temperature")
 
 func main() {
