2b7b6a6ed83aa55873bcf240b3bf937994f6ca11
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-10-24 23:46:28 -0400

    Finalize Chapter 07 exercise 8

--- chapter07/ch07ex08/main.go
+++ chapter07/ch07ex08/main.go
@@ -3,136 +3,44 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"sort"
-	"text/tabwriter"
-	"time"
 
-	"gopl/chapter07/ch07ex08/statefulsort"
+	"gopl/chapter07/ch07ex08/track"
 )
 
-// Track describes a music track on an album.
-type Track struct {
-	Title  string
-	Artist string
-	Album  string
-	Year   int
-	Length time.Duration
-}
-
-var tracks = []*Track{
-	{"Go", "Moby", "Moby", 1992, length("3m37s")},
-	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, length("4m24s")},
-	{"Go Ahead", "Alicia Keys", "As I Am", 2007, length("4m36s")},
-	{"Go", "Delilah", "From the Roots Up", 2012, length("3m38s")},
-}
-
-func length(s string) time.Duration {
-	d, err := time.ParseDuration(s)
-	if err != nil {
-		panic(s)
-	}
-	return d
-}
-
-// These functions behave like C's strcmp
-func cmpTitle(x, y *Track) int {
-	switch {
-	case x.Title < y.Title:
-		return -1
-	case x.Title == y.Title:
-		return 0
-	default:
-		return 1
-	}
-}
-
-func cmpArtist(x, y *Track) int {
-	switch {
-	case x.Artist < y.Artist:
-		return -1
-	case x.Artist == y.Artist:
-		return 0
-	default:
-		return 1
-	}
-}
-
-func cmpAlbum(x, y *Track) int {
-	switch {
-	case x.Album < y.Album:
-		return -1
-	case x.Album == y.Album:
-		return 0
-	default:
-		return 1
-	}
-}
-
-func cmpYear(x, y *Track) int {
-	switch {
-	case x.Year < y.Year:
-		return -1
-	case x.Year == y.Year:
-		return 0
-	default:
-		return 1
-	}
-}
-
-func cmpLength(x, y *Track) int {
-	switch {
-	case x.Length < y.Length:
-		return -1
-	case x.Length == y.Length:
-		return 0
-	default:
-		return 1
-	}
-}
-
-var (
-	cmpFuncs = []func(x, y interface{}) int{cmpTitle, cmpArtist, cmpAlbum, cmpYear, cmpLength}
-	names    = []string{"Title", "Artist", "Album", "Year", "Length"}
-)
-
-func printTracks(tracks []*Track) {
-	const format = "%v\t%v\t%v\t%v\t%v\t\n"
-	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
-	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
-	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
-	for _, t := range tracks {
-		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
-	}
-	tw.Flush() // calculate column widths and print table
+var tracks = []*track.Track{
+	{"Go", "Moby", "Moby", 1992, track.Length("3m37s")},
+	{"Ready 2 Go", "Martin Solveig", "Smash", 2011, track.Length("4m24s")},
+	{"Go Ahead", "Alicia Keys", "As I Am", 2007, track.Length("4m36s")},
+	{"Go", "Delilah", "From the Roots Up", 2012, track.Length("3m38s")},
 }
 
 func main() {
-	statefulTracks := statefulsort.NewStatefulSort(tracks, names, cmpFuncs)
+	statefulTracks := track.NewStatefulTracks(tracks)
 	fmt.Println("Initial order:")
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 	sort.Sort(statefulTracks)
 	fmt.Println("\nAfter sort:")
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 
 	fmt.Println("\nSet primary to Year")
 	statefulTracks.SetPrimary("Year")
 	sort.Sort(statefulTracks)
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 
 	fmt.Println("\nSet primary to Title")
 	statefulTracks.SetPrimary("Title")
 	sort.Sort(statefulTracks)
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 
 	fmt.Println("\nSet primary to Artist, then Title")
 	statefulTracks.SetPrimary("Artist")
 	statefulTracks.SetPrimary("Title")
 	sort.Sort(statefulTracks)
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 
 	fmt.Println("\nSet primary to Title again, reverse sort order")
 	statefulTracks.SetPrimary("Title")
 	sort.Sort(statefulTracks)
-	printTracks(statefulTracks.Elements())
+	track.PrintTracks(statefulTracks)
 }
---
+++ chapter07/ch07ex08/track/track.go
@@ -0,0 +1,160 @@
+// Package track provides Track, an implementation of the statefulsort
+// interface that represents a muscic track on an aobum.
+package track
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"text/tabwriter"
+	"time"
+
+	"gopl/chapter07/ch07ex08/statefulsort"
+)
+
+// Track describes a music track on an album.
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   int
+	Length time.Duration
+}
+
+// Length parses a string representing a duration. It can be used to simplify
+// initialization of a new track.
+func Length(s string) time.Duration {
+	d, err := time.ParseDuration(s)
+	if err != nil {
+		panic(s)
+	}
+	return d
+}
+
+// These functions behave like C's strcmp
+func cmpTitle(x, y interface{}) int {
+	xx, ok1 := x.(*Track)
+	yy, ok2 := y.(*Track)
+	if !ok1 || !ok2 {
+		log.Fatal("could not convert interface to *Track")
+	}
+	switch {
+	case xx.Title < yy.Title:
+		return -1
+	case xx.Title == yy.Title:
+		return 0
+	default:
+		return 1
+	}
+}
+
+func cmpArtist(x, y interface{}) int {
+	xx, ok1 := x.(*Track)
+	yy, ok2 := y.(*Track)
+	if !ok1 || !ok2 {
+		log.Fatal("could not convert interface to *Track")
+	}
+	switch {
+	case xx.Artist < yy.Artist:
+		return -1
+	case xx.Artist == yy.Artist:
+		return 0
+	default:
+		return 1
+	}
+}
+
+func cmpAlbum(x, y interface{}) int {
+	xx, ok1 := x.(*Track)
+	yy, ok2 := y.(*Track)
+	if !ok1 || !ok2 {
+		log.Fatal("could not convert interface to *Track")
+	}
+	switch {
+	case xx.Album < yy.Album:
+		return -1
+	case xx.Album == yy.Album:
+		return 0
+	default:
+		return 1
+	}
+}
+
+func cmpYear(x, y interface{}) int {
+	xx, ok := x.(*Track)
+	yy, ok := y.(*Track)
+	if !ok {
+		log.Fatal("could not convert interface to *Track")
+	}
+	switch {
+	case xx.Year < yy.Year:
+		return -1
+	case xx.Year == yy.Year:
+		return 0
+	default:
+		return 1
+	}
+}
+
+func cmpLength(x, y interface{}) int {
+	xx, ok := x.(*Track)
+	yy, ok := y.(*Track)
+	if !ok {
+		log.Fatal("could not convert interface to *Track")
+	}
+	switch {
+	case xx.Length < yy.Length:
+		return -1
+	case xx.Length == yy.Length:
+		return 0
+	default:
+		return 1
+	}
+}
+
+var (
+	cmpFuncs = []func(x, y interface{}) int{cmpTitle, cmpArtist, cmpAlbum, cmpYear, cmpLength}
+	names    = []string{"Title", "Artist", "Album", "Year", "Length"}
+)
+
+// NewStatefulTracks takes a slice of pointers to tracks as an argument and
+// returns a statefulsort for these tracks.
+func NewStatefulTracks(tracks []*Track) statefulsort.StatefulSort {
+	return statefulsort.NewStatefulSort(toIface(tracks), names, cmpFuncs)
+}
+
+// PrintTracks prints the tracks as they are currently sorted as a table.
+func PrintTracks(s statefulsort.StatefulSort) {
+	tracks := GetTracks(s)
+	const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	tw.Flush() // calculate column widths and print table
+}
+
+// GetTracks returns the track pointer slice for the statefulsort supplied as
+// the argument.
+func GetTracks(s statefulsort.StatefulSort) []*Track {
+	elements := s.Elements()
+	tracks := make([]*Track, len(elements))
+	for i, e := range elements {
+		t, ok := e.(*Track)
+		if !ok {
+			log.Fatal("could not convert interface to *Track")
+		}
+		tracks[i] = t
+	}
+	return tracks
+}
+
+func toIface(tracks []*Track) []interface{} {
+	ifaces := make([]interface{}, len(tracks))
+	for i, t := range tracks {
+		ifaces[i] = t
+	}
+	return ifaces
+}
