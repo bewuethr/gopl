3b022797ca32103e94bd5ac334802692e00781fe
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2018-11-09 00:12:22 -0500

    Add Chapter 07, exercise 10

---
+++ chapter07/ch07ex10/main.go
@@ -0,0 +1,30 @@
+// ch07ex10 implements a palindrome checker taking advantage of sort.Interface.
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func isPalindrome(s sort.Interface) bool {
+	j := s.Len() - 1
+	for i := 0; i < s.Len()/2; i++ {
+		if !(!s.Less(i, j) && !s.Less(j, i)) {
+			return false
+		}
+		j--
+	}
+	return true
+}
+
+func main() {
+	intsYes := []int{1, 2, 3, 2, 1}
+	intsNo := []int{1, 2, 3, 4, 5}
+	fmt.Printf("%v: %v\n", intsYes, isPalindrome(sort.IntSlice(intsYes)))
+	fmt.Printf("%v: %v\n", intsNo, isPalindrome(sort.IntSlice(intsYes)))
+
+	stringsYes := []string{"one", "two", "three", "two", "one"}
+	stringsNo := []string{"one", "two", "three", "four", "five"}
+	fmt.Printf("%v: %v\n", stringsYes, isPalindrome(sort.StringSlice(stringsYes)))
+	fmt.Printf("%v: %v\n", stringsNo, isPalindrome(sort.StringSlice(stringsNo)))
+}
