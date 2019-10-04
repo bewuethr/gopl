5fb7cba71782b3532a436f7c3aed0fbe3f78cd21
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2019-10-03 23:59:01 -0400

    Add Chapter 08, exercise 4

---
+++ chapter08/ch08ex04/main.go
@@ -0,0 +1,51 @@
+// Ch08ex04 is a TCP server that simulates an echo, using a sync.WaitGroup per
+// connection to let echo goroutines finish cleanly.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+	for input.Scan() {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			echo(c, f, 1*time.Second)
+		}(input.Text())
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	wg.Wait()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
