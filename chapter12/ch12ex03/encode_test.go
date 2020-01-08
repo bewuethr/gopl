0ea480a0b1966ef9f2f7ae5a2c883d87f30c034d
Benjamin Wuethrich <8521043+bewuethr@users.noreply.github.com>
2020-01-08 00:36:13 -0500

    Add map to test

--- chapter12/ch12ex03/encode_test.go
+++ chapter12/ch12ex03/encode_test.go
@@ -16,21 +16,22 @@ func TestMarshal(t *testing.T) {
 		want    []byte
 		wantErr error
 	}{
-		{8, []byte("8"), nil},                                     // int
-		{-8, []byte("-8"), nil},                                   // negative int
-		{uint(8), []byte("8"), nil},                               // uint
-		{"abc", []byte(`"abc"`), nil},                             // string
-		{&anInt, []byte("8"), nil},                                // pointer to int
-		{[]int{0, 1, 2}, []byte("(0 1 2)"), nil},                  // slice
-		{[...]int{0, 1, 2}, []byte("(0 1 2)"), nil},               // array
-		{[]int{}, []byte("()"), nil},                              // empty slice
-		{struct{ m, n int }{5, 6}, []byte("((m 5) (n 6))"), nil},  // struct
-		{true, []byte("t"), nil},                                  // true
-		{false, []byte("nil"), nil},                               // false
-		{3.14159, []byte("3.14159"), nil},                         // floating point
-		{1.1 + 2.1i, []byte("#C(1.1 2.1)"), nil},                  // complex number
-		{&anInterface, []byte(`("[]int" (1 2 3))`), nil},          // interface
-		{aFunc, []byte{}, fmt.Errorf("unsupported type: func()")}, // unsupported type
+		{8, []byte("8"), nil},                                              // int
+		{-8, []byte("-8"), nil},                                            // negative int
+		{uint(8), []byte("8"), nil},                                        // uint
+		{"abc", []byte(`"abc"`), nil},                                      // string
+		{&anInt, []byte("8"), nil},                                         // pointer to int
+		{[]int{0, 1, 2}, []byte("(0 1 2)"), nil},                           // slice
+		{[...]int{0, 1, 2}, []byte("(0 1 2)"), nil},                        // array
+		{[]int{}, []byte("()"), nil},                                       // empty slice
+		{struct{ m, n int }{5, 6}, []byte("((m 5) (n 6))"), nil},           // struct
+		{map[string]int{"a": 1, "b": 2}, []byte(`(("a" 1) ("b" 2))`), nil}, // map
+		{true, []byte("t"), nil},                                           // true
+		{false, []byte("nil"), nil},                                        // false
+		{3.14159, []byte("3.14159"), nil},                                  // floating point
+		{1.1 + 2.1i, []byte("#C(1.1 2.1)"), nil},                           // complex number
+		{&anInterface, []byte(`("[]int" (1 2 3))`), nil},                   // interface
+		{aFunc, []byte{}, fmt.Errorf("unsupported type: func()")},          // unsupported type
 	}
 
 	for _, test := range tests {
